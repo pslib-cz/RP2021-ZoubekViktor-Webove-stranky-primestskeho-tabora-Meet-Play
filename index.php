71a524209801857101ecf5bd95016f7217846984
viktorzoubek <81165815+viktorzoubek@users.noreply.github.com>
2021-03-22 14:12:09 +0100

    V1

---
+++ index.php
@@ -0,0 +1,257 @@
+<?php
+require_once "mailer.php";
+?>
+
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meet and Play</title>
+    <link rel="icon" href="./img/texturedlogo.png">
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/typography.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"
+        integrity="sha512-YUkaLm+KJ5lQXDBdqBqk7EVhJAdxRnVdT2vtCzwPHSweCzyMgYV/tgGF4/dCyqtCC2eCphz0lRQgatGVdfR0ww=="
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js"
+        integrity="sha512-UdIMMlVx0HEynClOIFSyOrPggomfhBKJE28LKl8yR3ghkgugPnG6iLfRfHwushZl1MOPSY6TsuBDGPK2X4zYKg=="
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
+        integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous">
+    </script>
+    <!--/*
+    ⠄⢀⣀⣤⣴⣶⣶⣤⣄⡀⠄⠄⣀⣤⣤⣤⣤⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+    ⣴⣏⣹⣿⠿⠿⠿⠿⢿⣿⣄⢿⣿⣿⣿⣿⣿⣋⣷⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+    ⣿⢟⣩⣶⣾⣿⣿⣿⣶⣮⣭⡂⢛⣭⣭⣭⣭⣭⣍⣛⣂⡀⠄⠄⠄⠄⠄⠄⠄⠄
+    ⣿⣿⣿⣿⡿⢟⣫⣭⣷⣶⣾⣭⣼⡻⢛⣛⣭⣭⣶⣶⣬⣭⣅⡀⠄⠄⠄⠄⠄⠄
+    ⣿⡿⢏⣵⣾⣿⣿⣿⡿⢉⡉⠙⢿⣇⢻⣿⣿⣿⣿⡟⠉⠉⢻⡷⠄⠄⠄⠄⠄⠄
+    ⣿⣷⣾⣍⣛⢿⣿⣿⣿⣤⣁⣤⣿⢏⠸⣿⣿⣿⣿⣷⣬⣥⣾⠁⣿⣿⣷⠄⠄⠄
+    ⣿⣿⣿⣿⣭⣕⣒⠿⠭⠭⠭⡷⢖⣫⣶⣶⣬⣭⣭⣭⣭⣥⡶⢣⣿⣿⣿⠄⠄⠄
+    ⣿⣿⣿⣿⣿⣿⣿⡿⣟⣛⣭⣾⣿⣿⣿⣝⡛⣿⢟⣛⣛⣁⣀⣸⣿⣿⣿⣀⣀⣀
+    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣿⡿⢛⣛⣛⣛⣙⣛⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣭⣭⠽⣛⢻⣿⣿⣿⠛⠛⠛
+    ⣿⢰⣿⣿⣿⣿⣟⣛⣛⣶⠶⠶⠶⣦⣭⣭⣭⣭⣶⡶⠶⣾⠟⢸⣿⣿⣿⠄⠄⠄
+    ⡻⢮⣭⣭⣭⣭⣉⣛⣛⡻⠿⠿⠷⠶⠶⠶⠶⣶⣶⣾⣿⠟⢣⣬⣛⡻⢱⣇⠄⠄
+    ⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⠶⠒⠄⠄⠄⢸⣿⢟⣫⡥⡆⠄⠄
+    ⢭⣭⣝⣛⣛⣛⣛⣛⣛⣛⣿⣿⡿⢛⣋⡉⠁⠄⠄⠄⠄⠄⢸⣿⢸⣿⣧⡅⠄⠄
+    ⣶⣶⣶⣭⣭⣭⣭⣭⣭⣵⣶⣶⣶⣿⣿⣿⣦⡀⠄⠄⠄⠄⠈⠡⣿⣿⡯⠁⠄⠄
+    */-->
+</head>
+
+<body>
+    <div class="backdropImage"></div>
+    <header class="menu">
+        <div class="burgerButton" onclick="menuToggle()">
+            <div class="burgerIcon"></div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#oprojektu" onclick="autoClose()">O PROJEKTU</a></li>
+                <li><a href="#nastym" onclick="autoClose()">NÁŠ TÝM</a></li>
+                <li><a href="#kurzy" onclick="autoClose()">KURZY</a></li>
+                <li><a href="#kontakt" onclick="autoClose()">KONTAKT</a></li>
+            </ul>
+        </nav>
+    </header>
+    <h1 hidden>Příměstský tábor Meet and Play</h1>
+    <div class="landing">
+        <figure class="logo">
+            <img src="./img/texturedlogo.png" alt="meet and play">
+            <figcaption>logo meet and play</figcaption>
+        </figure>
+        <div class="startButton"><a href="#contactForm">PŘIHLAŠ SE</a></div>
+    </div>
+    <main>
+        <article class="aboutProject" id="oprojektu">
+            <h2>O PROJEKTU</h2>
+            <p class="headline">Tento projekt byl vytvořen s velikým odhodláním usnadnit učení nejpoužívanějšího cizího
+                jazyka v ČR, a to přímo u nás v Liberci!</p>
+            <p>Koncept Meet&Play je založen především na známém mottu Jana Ámose Komenského Škola hrou a přesně v tomto
+                duchu jsme připravili veškeré aktivity, se kterými se malí nadšení angličtináři mohou na našich
+                příměstských táborech setkat.</p>
+            <p>Nejdůležitějším cílem naší práce je vhodná a velmi potřebná motivace dětí, bez ohledu na věk či jazykovou
+                úroveň. Posláním je pro nás především zpřístupnění možnosti učení cizího jazyka pro každého malého
+                zájemce.</p>
+            <p>V roce 2021 jsme navázali spolupráci s katedrou anglického jazyka <a href="https://kaj.fp.tul.cz/">Technické univerzity v
+                    Liberci</a>.<br />
+                Díky této spolupráci Vám můžeme nabídnout anglický příměstský tábor, kde bude přítomný rodilý mluvčí
+                <b>Christopher Muffett</b>.</p>
+            <figure>
+                <img src="./img/logo_technicka_univerzita.png" alt="technická univerzita v liberci">
+                <figcaption>logo TUL</figcaption>
+            </figure>
+        </article>
+        <article class="ourTeam" id="nastym">
+            <h2>NÁŠ TÝM</h2>
+            <p class="headline">Jsme kreativní dvojka, která se výuce anglického jazyka věnuje již několik let, a to jak
+                na základní škole, tak formou individuální výuky klientů různého věku.</p>
+            <p>Ivet v roce 2020 úspěšně dokončila bakalářské studium na katedře angličtiny v Liberci a nyní je
+                studentkou navazujícího magisterského studia. Stefi dokončila magisterské studium také na katedře
+                anglického jazyka v roce 2020.</p>
+            <p>Cílem nás obou je vytvářet takové podmínky k učení, aby žádná lekce nebyla nuda!</p>
+            <p>Mezi naše oblíbené metody patří výuka formou myšlenkových map, pohybových aktivit či tematických
+                projektů. Cílem naší práce je zainteresovat každého jedince. Kromě využívání moderních technologií velmi
+                často využíváme vlastnoručně vyrobené pomůcky.</p>
+            <p>Pokud s námi chcete zůstat v kontaktu nebo se dozvědět perličky z angličtiny, určitě koukněte na náš <a
+                    href="https://www.instagram.com/_meetandplay_/">instagram</a> a <a href="https://www.facebook.com/meetandplay1">facebook</a>.</p>
+            <figure>
+                <img src="./img/foto_OUR_TEAM.jpg" alt="náš tým">
+                <figcaption>our team photo</figcaption>
+            </figure>
+        </article>
+        <article class="courses" id="kurzy">
+            <h2>KURZY</h2>
+            <p><b>Proč právě k nám?</b> Důvodů je hned několik! Jsme mladá kreativní dvojka plná energie, se kterou se
+                rozhodně nebudete nudit! Máme pro Vás připravený nabitý program, díky kterému si odnesete nové znalosti,
+                zkušenosti a užijeme si spoustu legrace!</p>
+            <section>
+                <div class="sectionHeader">
+                    <h3>ANGLICKÝ PŘÍMĚSTSKÝ TÁBOR</h3>
+                </div>
+                <p>Rády bychom Vám nabídly možnost přihlásit Vaše děti na anglický příměstský tábor, který se bude konat
+                    v červenci a to hned ve dvou termínech. První termín je od 12. – 16. 7. 2021 a druhý termín
+                    je od 19. – 23. 7. 2021.</p>
+                <p>Co určitě nesmíme zapomenout zmínit, je přítomnost <b>rodilého mluvčího</b>, který s námi díky
+                    spolupráci s Technickou univerzitou stráví celý týden! Jeho jméno je <b>Christopher Muffett</b>.</p>
+                <p>Christopher se narodil v Londýně a jeho velkou zálibou je zpěv. Pokud se o něm chcete dovědět něco
+                    víc, podívejte se <a href="https://kaj.fp.tul.cz/staff/userprofile/muffett">sem</a>.</p>
+                <p><b>Kdo se může tábora zúčastnit?</b> Účastnit se může každý, kdo chce! Budeme rády, když dětem bude 7
+                    – 15 let. Pokud by měl zájem i ten, komu ještě nebylo 7 a má již nějaké zkušenosti s angličtinou, je
+                    také vítán!</p>
+                <p><b>A jak bude takový anglický příměstský tábor probíhat?</b> Sejdeme se každý den v 8 hodin ráno a od
+                    8:30 se ze všech koutů začnou ozývat anglická slovíčka a fráze. Okolo 12 hodiny si dáme oběd v
+                    jídelně s krátkou pauzou. Po obědě se můžete těšit na odpolední program, který bude končit v 15:30.
+                </p>
+                <p>Cena příměstského tábora na jeden týden je <b>3.299Kč</b>. V ceně je zahrnuta jazyková výuka, veškeré
+                    pomůcky, obědy, svačiny a pitný režim. Pokud bude odpolední program vyžadovat vstup, může být
+                    účtován zvlášť.</p>
+                <p class="headline">Vyplňte přihlášku a staňte se součástí letního anglického dobrodružství!</p>
+            </section>
+        </article>
+        <form id="contactForm" class="contact-form" method="post" novalidate>
+            <div>
+                <label>JMÉNO ÚČASTNÍKA</label>
+                <input class="form-control" type="text" name="name" placeholder="Jméno účastníka" required><br>
+            </div>
+            <div>
+                <label>DATUM NAROZENÍ ÚČASTNÍKA</label>
+                <input class="form-control" type="date" name="birthdate" required><br>
+            </div>
+            <div class="date">
+                <div class="dateOne">
+                    <input type="radio" name="coursedate" id="firstDate" value="1" checked>
+                    <label for="firstDate">12. - 16.7. 2021</label>
+                </div>
+                <div class="dateTwo">
+                    <input type="radio" name="coursedate" id="secondDate" value="2">
+                    <label for="secondDate">19. - 23.7. 2021</label>
+                </div>
+            </div>
+            <div>
+                <label>JMÉNO ZÁKONNÉHO ZÁSTUPCE</label>
+                <input class="form-control" type="text" name="parentname" placeholder="Jméno zástupce" required><br>
+            </div>
+            <div>
+                <label>KONTAKTNÍ E-MAIL</label>
+                <input class="form-control" type="email" name="email" placeholder="meetandplay@email.cz" required><br>
+            </div>
+            <div>
+                <label>KONTAKTNÍ TELEFONNÍ ČÍSLO</label>
+                <input class="form-control" type="tel" name="tel" placeholder="+420 123 456 789" required><br>
+            </div>
+            <button type="submit" name="submit">LET'S PLAY!</button>
+        </form>
+        <div>
+            <div id="statusMessage"><?php echo $statusMessage;?></div>
+        </div>
+    </main>
+    <footer>
+        <div id="kontakt">
+            <h2>KONTAKT</h2>
+            <a href="tel:+420607715751">+420 607 715 751</a><br>
+            <a href="mailto:kontakt@meetandplay.cz">kontakt@meetandplay.cz</a><br>
+            <a href="https://kaj.fp.tul.cz/">Katedra anglického jazyka</a>
+            <div class="icons">
+                <a href="https://www.facebook.com/meetandplay1" class="icon icon-facebook2"></a>
+                <a href="https://www.instagram.com/_meetandplay_/" class="icon icon-instagram"></a>
+            </div>
+        </div>
+        <figure>
+            <img src="./img/logo_meetandplay.svg" alt="">
+            <figcaption>logo meet and play</figcaption>
+        </figure>
+    </footer>
+    <div class="credits"><span>website created by <a href="https://www.instagram.com/viktoranriil/">Viktor Zoubek</a></span><span><a href="mailto:viktorzoubek7@gmail.com">contact me</a></span></div>
+    <script>
+        const burgerBtn = document.querySelector(".burgerButton");
+        const menuBall = document.querySelector(".menu");
+        let menuOpen = false;
+
+        function menuToggle() {
+            if (!menuOpen) {
+                burgerBtn.classList.toggle("open");
+                menuBall.classList.toggle("open");
+                menuOpen = true;
+            } else {
+                burgerBtn.classList.toggle("open");
+                menuBall.classList.toggle("open");
+                menuOpen = false;
+            }
+        }
+
+        function autoClose() {
+            if (menuOpen) {
+                burgerBtn.classList.toggle("open");
+                menuBall.classList.toggle("open");
+                menuOpen = false;
+            }
+        }
+    </script>
+    <script>
+        $(document).ready(function () {
+            $("#contactForm").validate({
+                rules: {
+                    "name": "required",
+                    "birthdate": "required",
+                    "coursedate": "required",
+                    "parentname": "required",
+                    "email": "required",
+                    "tel": "required"
+                },
+                messages: {
+                    "name": "Povinné pole",
+                    "birthdate": "Povinné pole",
+                    "coursedate": "Povinné pole",
+                    "parentname": "Povinné pole",
+                    "email": "Povinné pole",
+                    "tel": "Povinné pole",
+                },
+                errorElement: "em",
+                errorPlacement: function (error, element) {
+                    error.addClass("invalid-feedback");
+
+                    if (element.prop("type") === "checkbox") {
+                        error.insertAfter(element.next("label"));
+                    } else {
+                        error.insertAfter(element);
+                    }
+                },
+                highlight: function (element, errorClass, validClass) {
+                    $(element).addClass("is-invalid").removeClass("is-valid");
+                },
+                unhighlight: function (element, errorClass, validClass) {
+                    $(element).addClass("is-valid").removeClass("is-invalid");
+                }
+            });
+        });
+    </script>
+</body>
+</html>
---
+++ mailer.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * This example shows sending a message using a local sendmail binary.
+ */
+
+//Import the PHPMailer class into the global namespace
+//PHPMailer dependecies
+
+$statusMessage = "";
+$parentname = "";
+$contactemail = "";
+$contactnumber = "";
+$subject = "";
+
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+require 'PHPMailer-master/src/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+try{
+    //Create a new PHPMailer instance
+    $mail = new PHPMailer(true);
+    
+    if(isset($_POST["submit"])){
+    
+    //set correct encoding and language
+    $mail->CharSet = "UTF-8";
+    $mail->Encoding = 'base64';
+    $mail->setLanguage('cs', 'PHPMailer-master/language/');
+    
+    //confirmation html message
+    date_default_timezone_set('Europe/Prague');
+    $dateTime = date('d.m.Y H:i:s', time());
+
+    $name = $_POST["name"];
+    $birthdate = $_POST["birthdate"];
+    $datenumber = $_POST["coursedate"];
+    $parentname = $_POST["parentname"];
+    $contactemail = $_POST["email"];
+    $contactnumber = $_POST["tel"];
+
+    $subject = "Nová registrace od " . $parentname;
+    
+    //encode subject
+    $preferences = ['input-charset' => 'UTF-8', 'output-charset' => 'UTF-8'];
+    $encoded_subject = iconv_mime_encode('Subject', $subject, $preferences);
+    $encoded_subject = substr($encoded_subject, strlen('Subject: '));
+    
+    if ($datenumber = 1){
+        $date = "12. - 16. 7. 2021";
+    } else{
+        $date = "19. - 23. 7. 2021";
+    }
+
+    $message = '<html><body style="font-size: 16px;">';
+    $message .= '<img style="max-width:120px" src="https://meetandplay.cz/img/texturedlogo.png" alt="meetandplay logo" />';
+    $message .= '<h1 style="font-family: Arial, Helvetica, sans-serif; font-size: 24px;">Nová registrace Meet and Play</h1>';
+    $message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+    $message .= "<tr style='background: #f2f2f2;'><td><strong>Jméno účastníka:</strong> </td><td>" . $name . "</td></tr>";
+    $message .= "<tr><td><strong>Datum narození účastníka:</strong> </td><td>" . $birthdate . "</td></tr>";
+    $message .= "<tr style='background: #f2f2f2;'><td><strong>Vybraný termín:</strong> </td><td>" . $date . "</td></tr>";
+    $message .= "<tr><td><strong>Jméno zákonného zástupce:</strong> </td><td>" . $parentname . "</td></tr>";
+    $message .= "<tr style='background: #f2f2f2;'><td><strong>Kontaktní e-mail:</strong> </td><td>" . $contactemail . "</td></tr>";
+    $message .= "<tr><td><strong>Kontaktní telefon:</strong> </td><td>" . $contactnumber . "</td></tr>";
+    $message .= "</table>";
+    $message .= "<p>Tato zpráva byla vygenerována pomocí systému PHPMailer</p>";
+    $message .= "</body></html>";
+    
+    $mail->SMTPDebug = 0;
+    $mail->IsSMTP();
+    $mail->Host = 'mail.hukot.net';
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->Username = "form@meetandplay.cz";
+    $mail->Password = "69420";
+    $mail->SMTPSecure = 'tls';
+    
+    //Set PHPMailer to use the sendmail transport
+    //$mail->isSendmail();
+    //Set who the message is to be sent from
+    $mail->setFrom($contactemail, $parentname);
+    //Set an alternative reply-to address
+    //$mail->addReplyTo('replyto@example.com', 'First Last');
+    //Set who the message is to be sent to
+    $mail->addAddress('kontakt@meetandplay.cz');
+    //Set the subject line
+    $mail->Subject = $encoded_subject;
+    //Read an HTML message body from an external file, convert referenced images to embedded,
+    //convert HTML into a basic plain-text alternative body
+    $mail->msgHTML($message);
+    //Replace the plain text body with one created manually
+    //$mail->AltBody = 'This is a plain-text message body';
+    //Attach an image file
+    //$mail->addAttachment('images/phpmailer_mini.png');
+    
+    //send the message, check for errors
+    if (! $mail->Send()) {
+        //mailer error
+        $statusMessage = "<div class='form-group row mb-3'><div class='col-md-12' id='statusMessage'><div class='alert alert-danger'><span class='mr-1'><i class='fas fa-times-circle'></i></span>Chyba Maileru: Přihlášku se nepodařilo odeslat. Zkuste to prosím znovu, nebo nás kontaktujte. Error: " . $mail->ErrorInfo . "</div></div></div>";
+        
+    } else {
+        //success message
+        $statusMessage = "<div class='form-group row mb-3'><div class='col-md-12' id='statusMessage'><div class='alert alert-success'><span class='mr-1'><i class='fas fa-check'></i></span>Registrace byla úspěšná, brzy budete kontaktováni. </div></div></div>";
+    }
+    }
+//catch and output exceptions
+} catch (Exception $e) {
+    $e->errorMessage();
+    $statusMessage = "<div class='form-group row mb-3'><div class='col-md-12' id='statusMessage'><div class='alert alert-danger'><span class='mr-1'><i class='fas fa-times-circle'></i></span>Error: Objednávku se nepodařilo odeslat. Podrobnosti: " . $e . "</div></div></div>";
+
+} catch (\Exception $e) {
+    $e->getMessage();
+    $statusMessage = "<div class='form-group row mb-3'><div class='col-md-12' id='statusMessage'><div class='alert alert-danger'><span class='mr-1'><i class='fas fa-times-circle'></i></span>Error: Objednávku se nepodařilo odeslat. Podrobnosti: " . $e . "</div></div></div>";
+}
